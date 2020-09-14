661581e04184ec8933b11ed113102a6364bd434d
jgarcia15121982 <johfregarcia@gmail.com>
2020-09-14 17:26:39 -0500

    [FIX] Fixing PEP8

--- 0x15-api/0-gather_data_from_an_API.py
+++ 0x15-api/0-gather_data_from_an_API.py
@@ -7,6 +7,7 @@ import requests
 import sys
 
 if __name__ == "__main__":
+
     resp_1 = requests.get("https://jsonplaceholder.typicode.com/users/" + 
                           sys.argv[1])
     usr_dict = json.loads(resp_1.text)
