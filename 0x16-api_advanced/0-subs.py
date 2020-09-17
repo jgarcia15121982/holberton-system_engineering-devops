585d458905d9f251ffe39c41710d2670964aa9cd
jgarcia15121982 <johfregarcia@gmail.com>
2020-09-17 12:32:52 -0500

    [FIX] Fixing function

--- 0x16-api_advanced/0-subs.py
+++ 0x16-api_advanced/0-subs.py
@@ -7,8 +7,8 @@ def number_of_subscribers(subreddit):
     """returns the number of subscribers"""
     headers = {"User-Agent": "jgarcia"}
     resp = requests.get('https://reddit.com/r/' + subreddit + '/about.json',
-                        headers=headers, allow_redirects=True)
-    if subs.status_code == 404:
+                        headers=credentials, allow_redirects=False)
+    if resp.status_code != 200:
         return 0
     subs_json = subs.json()
     subs_data = subs_json['data']
