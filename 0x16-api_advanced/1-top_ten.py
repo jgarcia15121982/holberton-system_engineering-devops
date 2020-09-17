08078e3d1db9a7bf78cb93483ea716a8fbacb4c4
jgarcia15121982 <johfregarcia@gmail.com>
2020-09-17 12:52:48 -0500

    [ADD] Top Ten (true)

---
+++ 0x16-api_advanced/1-top_ten.py
@@ -0,0 +1,27 @@
+#!/usr/bin/python3
+""" Top Ten """
+import requests
+
+
+def top_ten(subreddit):
+    """function that queries the Reddit API
+       and prints the titles of the first 10
+       hot posts listed for a given subreddit"""
+    
+    base_url = 'https://www.reddit.com/r/'
+    url = base_url + subreddit + "/hot.json"
+    credentials = {'User-Agent': "linux:1:v1.0 (by /u/svelezg_r)"}
+    max_number = {'limit': "10"}
+    response = requests.get(url,
+                            headers=credentials,
+                            params=max_number,
+                            allow_redirects=False)
+
+    if response.status_code != 200:
+        print(None)
+    else:
+        hot_list_of_dicts = response.json().get("data").get("children")
+        titles = [reddit.get("data").get("title") for
+                  reddit in hot_list_of_dicts]
+        for title in titles:
+            print(title)
